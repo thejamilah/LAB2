ca570060ff0f62b89a11e41faaf6e10ba7f3a177
Jamilah McWilliams <jamilahmcw@gmail.com>
2018-06-12 15:38:55 -0700

    completed lab2

--- app.js
+++ app.js
@@ -1,90 +1,90 @@
-'use strict';
+// 'use strict';
 
-var userPoints = 0;
+// var userPoints = 0;
 
-while(!user) {
-var user = prompt('Hi, What is your name?');
-}
+// while(!user) {
+// var user = prompt('Hi, What is your name?');
+// }
 
- console.log(user);
+//  console.log(user);
 
- alert('Hi, ' + user + '!');
+//  alert('Hi, ' + user + '!');
 
- var question1 = prompt('Shall we play a game? (Pay attention there\'s a question that references this later!) Please answer question with Y/N or YES/NO').toLowerCase();
+//  var question1 = prompt('Shall we play a game? (Pay attention there\'s a question that references this later!) Please answer question with Y/N or YES/NO').toLowerCase();
 
- console.log(question1);
+//  console.log(question1);
 
- if (question1 === 'yes' || question1 === 'y') {
-     //if answer is yes alert
+//  if (question1 === 'yes' || question1 === 'y') {
+//      //if answer is yes alert
 
-     var question1 = alert('Great! Let\'s play! Press Ok!');
+//      var question1 = alert('Great! Let\'s play! Press Ok!');
 
- } else {
-     //if answer is no alert with movie quote
-     alert('The only winning move is not to play.');
- }
+//  } else {
+//      //if answer is no alert with movie quote
+//      alert('The only winning move is not to play.');
+//  }
 
- var question2 = prompt('What movie was the previous questions quoted from?');
+//  var question2 = prompt('What movie was the previous questions quoted from?');
 
- if (question2 === 'War Games' || question2 === 'war games') {
+//  if (question2 === 'War Games' || question2 === 'war games') {
 
-     console.log(question2);
+//      console.log(question2);
 
-     userPoints =+1;
+//      userPoints =+1;
 
-     var question2 = alert('Congrats cult movie buff!') 
+//      var question2 = alert('Congrats cult movie buff!') 
 
- } else {
+//  } else {
 
-     alert('Ruh roh...wrong answer. The correct answer is War Games staring a young Matthew Broderick. Click OK and keep going to get to know more About Me!');
- }
+//      alert('Ruh roh...wrong answer. The correct answer is War Games staring a young Matthew Broderick. Click OK and keep going to get to know more About Me!');
+//  }
 
- alert(user + ' you have ' + userPoints + ' points!');
+//  alert(user + ' you have ' + userPoints + ' points!');
 
- var question3 = prompt('Does Jamilah love to travel? Y/N').toLowerCase();
+//  var question3 = prompt('Does Jamilah love to travel? Y/N').toLowerCase();
 
- if (question3 === 'yes' || question3 === 'y') {
+//  if (question3 === 'yes' || question3 === 'y') {
 
-     console.log(question3);
+//      console.log(question3);
 
-     userPoints +=1;
+//      userPoints +=1;
 
-     var question3 = alert('Yes! Will travel for FOOD and other cool stuff too.')
- } else {
+//      var question3 = alert('Yes! Will travel for FOOD and other cool stuff too.')
+//  } else {
     
-     alert('Wrong answer. Are you crazy?! I LOVE to travel!');
- }
+//      alert('Wrong answer. Are you crazy?! I LOVE to travel!');
+//  }
 
- alert(user + ' you have ' + userPoints + ' points!');
+//  alert(user + ' you have ' + userPoints + ' points!');
 
- var question4 = prompt('Does Jamilah love to cook? Y/N').toLowerCase();
+//  var question4 = prompt('Does Jamilah love to cook? Y/N').toLowerCase();
 
- if (question4 === 'yes' || question4 === 'y') {
+//  if (question4 === 'yes' || question4 === 'y') {
 
-     console.log(question4);
+//      console.log(question4);
 
-     userPoints += 1;
+//      userPoints += 1;
 
-     var question4 = alert('I love to eat and I Love to cook. Fun fact...I am a culinary school grad.')
- } else {
-     alert('Wrong answer, NO FOOD FOR YOU!')
- }
+//      var question4 = alert('I love to eat and I Love to cook. Fun fact...I am a culinary school grad.')
+//  } else {
+//      alert('Wrong answer, NO FOOD FOR YOU!')
+//  }
 
- alert(user + ' you have ' + userPoints + ' points!');
+//  alert(user + ' you have ' + userPoints + ' points!');
 
- var question5 = prompt('Does Jamilah love to workout?').toLowerCase();
+//  var question5 = prompt('Does Jamilah love to workout?').toLowerCase();
 
- if (question5 === 'yes' || question5 === 'y') {
+//  if (question5 === 'yes' || question5 === 'y') {
 
-     console.log(question5);
+//      console.log(question5);
 
-     userPoints += 1;
+//      userPoints += 1;
 
-     var question5 = alert('I wouldn\'t say LOVE but I do when I have time.')
+//      var question5 = alert('I wouldn\'t say LOVE but I do when I have time.')
 
- } else {
+//  } else {
 
-     alert('You are right! I do not LOVE it but I like to eat so...')
- }
+//      alert('You are right! I do not LOVE it but I like to eat so...')
+//  }
 
- alert(user + ' you have ' + userPoints + ' points!');
+//  alert(user + ' you have ' + userPoints + ' points!');
